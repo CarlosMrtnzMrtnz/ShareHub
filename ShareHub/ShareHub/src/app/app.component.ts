2b773bace2ebd4ca7d69aaa0cfa578bee2e65fcf
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-02-07 19:01:36 -0500

    creacion de componentes

--- ShareHub/ShareHub/src/app/app.component.ts
+++ ShareHub/ShareHub/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent} from '../app/component/inicio/inicio.component'
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { MenuIzquierdaComponent } from './component/menu-izquierda/menu-izquierda.component';
+import { MenuDerechaComponent } from './component/menu-derecha/menu-derecha.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NavbarComponent,
+    MenuIzquierdaComponent,
+    MenuDerechaComponent
+
+
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ShareHub/ShareHub/src/app/app.routes.ts
+++ ShareHub/ShareHub/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InicioComponent} from '../app/component/inicio/inicio.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',title:"Inicio",component:InicioComponent}
+];
