c099e81fbc12aeda36de7272f77fa15da55fda5f
Paula <paulaoorjuela@gmail.com>
2024-02-12 12:38:35 -0500

    add apiShareHub folder. add controllers, models, config, middleware, routes folders y files. add config.env, index.js y editorConfig

---
+++ apiShareHub/controllers/publicacionesController.js
@@ -0,0 +1,1 @@
+const publicacionesModel = require('../models/publicacionesModel')
---
+++ apiShareHub/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db')
+const cors = require('cors')
+
+
+connectDB()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/api', require('./routes/routes'))
+
+app.listen(4000, () => {
+    console.log('Servidor ejecutándose en el puerto 4000');
+})
