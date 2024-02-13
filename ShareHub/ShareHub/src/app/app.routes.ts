5fcf5f6032c48bfaf32fe185a2329319ea09d842
Celussky <gabri.bernal@gmail.com>
2024-02-12 19:09:27 -0500

    Finalización del componente menu-izquierda con mediaqueries

--- ShareHub/ShareHub/src/app/app.routes.ts
+++ ShareHub/ShareHub/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { InicioComponent} from '../app/component/inicio/inicio.component'
+import { InicioComponent} from '../app/components/inicio/inicio.component'
+import { PerfilComponent } from './components/perfil/perfil.component'
 
 export const routes: Routes = [
-    {path:'',title:"Inicio",component:InicioComponent}
+    {path:'',title:"Inicio",component:InicioComponent},
+    {path: 'perfil', title: "Perfil | ShareHub", component:PerfilComponent}
 ];
