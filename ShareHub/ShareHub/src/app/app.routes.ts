3497bcd058c53d931c85142ea59cb0ef9361c33e
Paula <paulaoorjuela@gmail.com>
2024-02-15 18:19:49 -0500

    add component registro grupos, multer

--- ShareHub/ShareHub/src/app/app.routes.ts
+++ ShareHub/ShareHub/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { GruposComponent } from "../app/components/grupos/grupos.component";
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
 import { PerfilComponent } from './components/perfil/perfil.component'
+import { RegistroGruposComponent } from "./components/registro-grupos/registro-grupos.component";
 
 const tituloPagina = "ShareHub"
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path:'perfil',title: `Perfil | ${tituloPagina}`,component:PerfilComponent},
     {path:'',title:"login",component:LoginComponent},
     {path:'inicio',title:"Inicio de sesion",component:InicioComponent},
-    {path: 'registro', component: RegistroComponent }
+    {path:'registro', component: RegistroComponent },
+    {path:'registro-grupos', title: `Registro Grupos | ${tituloPagina}`, component:RegistroGruposComponent}
 ];
---
+++ ShareHub/ShareHub/src/app/services/sharehub-api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SharehubApiService {
+    private http = inject(HttpClient);
+    private urlApi: string = 'http://localhost:4000/api';
+
+    constructor() {}
+
+    //   -------------------------- SERVICE GRUPOS --------------------------
+    getGrupos() {
+        return this.http.get(this.urlApi + '/consultar-grupos');
+    }
+
+    getUnGrupo(grupoId: string) {
+        return this.http.get(`${this.urlApi}/consultar-grupo${grupoId}`);
+    }
+
+    postGrupo(dataGrupo:any){
+        return this.http.post(`${this.urlApi}/crear-grupo`, dataGrupo)
+    }
+
+    deleteGrupo(grupoId:string){
+        return this.http.delete(`${this.urlApi}/eliminar-grupo/${grupoId}`)
+    }
+
+    putGrupo(grupoId:string, dataGrupo:any){
+        return this.http.put(`${this.urlApi}/actualizar-grupo/${grupoId}`, dataGrupo)
+    }
+}
