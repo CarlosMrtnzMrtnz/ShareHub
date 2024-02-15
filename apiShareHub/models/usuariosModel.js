25c447fbc6feafe63015ae014817cc180f00487b
Lguz12 <layhan2913@gmail.com>
2024-02-15 18:13:45 -0500

    controladores y modelos user

--- apiShareHub/models/usuariosModel.js
+++ apiShareHub/models/usuariosModel.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose')
+
+const usuariomodelo =mongoose.Schema({
+    nombreuser: {
+        type: String,
+        require: true
+    },
+    CorreoUser:{
+        type: String,
+        require: true
+    },
+    clave:{
+        type: String,
+        require: true
+    },
+    imguser:{
+        type : String,
+        require: false
+    },
+    descripcionuser: {
+        type: String,
+        require: false
+    }
+},{
+    timestamps :true,
+    versionkey:false    
+})
+module.exports = mongoose.model('usuario', usuariomodelo)
--- apiShareHub/routes/routes.js
+++ apiShareHub/routes/routes.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const usuarioController =require('../controllers/usuarios.Controller')
+
+const mdJWT = require('../middleware/jwt')
+
+// ?Rutas Uauario
+router.get('/consultar-usuario', usuarioController.consultarUsuarios);
+router.get('/consultar-usuario/:usuarioId', usuarioController.consultarUnUsuario);
+router.post('/crear-usuario', usuarioController.crearUsuario)
+router.put('/actualizar-usuario/:usuarioId', usuarioController.actualizarUsuario)
+router.delete('/eliminar-usuario/:usuarioId', usuarioController.eliminarUsuario)
+
+module.exports = router
+
