9beeb60f52c56f3396fe0a2ac8bd282dd991152d
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-02-15 10:23:39 -0500

    creacion de rutas

--- apiShareHub/routes/routes.js
+++ apiShareHub/routes/routes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const publicacionController = require ('../controllers/publicacionesController')
+
+
+router.post('/publicacion', publicacionController.crearPulicacion);
+router.delete('/eliminar-publicacion/:idProducto', publicacionController.eliminarPublicacion)
+
+module.exports = router
