58d015a70de2a743578b6b9dcf0e0781491991cd
Celussky <gabri.bernal@gmail.com>
2024-02-15 19:01:55 -0500

    perfil component css

--- ShareHub/ShareHub/src/app/components/menu-izquierda/menu-izquierda.component.spec.ts
+++ ShareHub/ShareHub/src/app/components/menu-izquierda/menu-izquierda.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuIzquierdaComponent } from './menu-izquierda.component';
 
 describe('MenuIzquierdaComponent', () => {
-  let component: MenuIzquierdaComponent;
-  let fixture: ComponentFixture<MenuIzquierdaComponent>;
+    let component: MenuIzquierdaComponent;
+    let fixture: ComponentFixture<MenuIzquierdaComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MenuIzquierdaComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(MenuIzquierdaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [MenuIzquierdaComponent]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(MenuIzquierdaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
