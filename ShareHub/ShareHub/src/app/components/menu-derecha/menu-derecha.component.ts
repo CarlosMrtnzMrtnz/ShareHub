aabda4624d230f9575474e078aa4649a7424dff6
Lguz12 <layhan2913@gmail.com>
2024-02-27 18:14:21 -0500

    merge 27 feb

--- ShareHub/ShareHub/src/app/components/menu-derecha/menu-derecha.component.ts
+++ ShareHub/ShareHub/src/app/components/menu-derecha/menu-derecha.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { SharehubApiService } from '../../services/sharehub-api.service';
@@ -15,16 +15,22 @@ import { SharehubApiService } from '../../services/sharehub-api.service';
   styleUrl: './menu-derecha.component.css'
 })
 export class MenuDerechaComponent {
-    miladitoderecho = signal([
-        { ruta1: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg',nombre: "la suc"},
-        { ruta1: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg',nombre:"ciona"},
-        { ruta1: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg",nombre:"por"},
-        { ruta1: 'https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg',nombre:"la noche"},
-
-      ])
+    userdata = signal<any>([])
+    private productosServices = inject(SharehubApiService)
+    @Input() nombre!: string
+    @Input() imguser!: string
 
 
+    ngOnInit(){
+        this.productosServices.getUsuarios().subscribe({
+            next: (user) => {
+                this.userdata.set(user)
+                console.log(this.userdata());
 
-
-
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        })
+    }
 }
