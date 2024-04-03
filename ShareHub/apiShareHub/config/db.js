71f189e01ceb2b6a1da86ca42374a19e937b6781
Paula <paulaoorjuela@gmail.com>
2024-04-02 20:38:09 -0500

    update base de datos con docker

--- ShareHub/apiShareHub/config/db.js
+++ ShareHub/apiShareHub/config/db.js
@@ -4,20 +4,14 @@ require('dotenv').config({ path: 'config.env' });
 const connectDB = async () => {
     try {
         let mongoURI = process.env.DB_MONGO;
-        if (process.env.NODE_ENV === 'docker') {
-            mongoURI = 'mongodb://host.docker.internal:27017/mydatabase';
-        }
-        
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        
+
         console.log('Conectado a la base de datos');
     } catch (error) {
         console.error('Error al conectar a la base de datos:', error);
-        // No es recomendable volver a llamar a connectDB() si hay un error
-        // connectDB();
     }
 };
 
